c1d39e56d2d8a8b74bea3f9d3992f1702bb0c8b8
Roll20 <cycla4@gmail.com>
2022-09-01 17:20:49 +0800

    fix packages

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './style.css';
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 import {
   OrbitControls
-} from 'three/examples/jsm/controls/OrbitControls.js';
+} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
 
 
 // Scene
