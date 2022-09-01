eb76f87a0f64f57b6278d04683523cebf1572038
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-09-01 17:35:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import './style.css';
-import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
-import {
-  OrbitControls
-} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";
 
 
 // Scene
